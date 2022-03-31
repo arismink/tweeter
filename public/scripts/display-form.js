0153bbc59c1f92fa1df6361df498d483853fcbd7
krismina la <krismina.la@gmail.com>
2022-03-31 16:50:11 +0000

    Adds script that toggles tweet form when user interacts with icon

--- public/scripts/display-form.js
+++ public/scripts/display-form.js
@@ -1,15 +1,18 @@
 /*
 
-  script to toggle tweet form
+  Script that toggles tweet form when user interacts with double arrow icon
 
 */
 
+// get mentor to review this
+
 $(() => {
-  // hide new-tweet form upon load
+  // Hides tweet-form upon load
   $('#new-tweet').hide();
 
-  // display new-tweet when user clicks arrow
+  // Display form when arrow is clicked
   $('#tweet-arrow').on('click', function() {
     $('#new-tweet').slideDown();
+    $('#tweet-text').focus();
   })
 })
