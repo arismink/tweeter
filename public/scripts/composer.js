9b5c0783bd115ace8e7591a008133e934b12ce4d
krismina la <krismina.la@gmail.com>
2022-04-01 02:25:29 +0000

    fades in/out whole navbar instead of just new tweet text

--- public/scripts/composer.js
+++ public/scripts/composer.js
@@ -14,12 +14,12 @@ $(() => {
   $(window).on('scroll', function() {
 
     if ($(this).scrollTop() !== 0) { // If user is not at top of page
-      $('#arrow-scroll').fadeIn("fast"); // Show scroll icon
-      $('.navbar2').fadeOut("fast"); // Hide tweet icon
-
+      $('#arrow-scroll').fadeIn('fast'); // Show scroll icon
+      $('nav').fadeOut('slow'); // Hide nav bar
+      
     } else {
-      $('#arrow-scroll').fadeOut("fast"); // Hide scroll icon
-      $('.navbar2').fadeIn("fast"); // Show tweet icon
+      $('#arrow-scroll').fadeOut('fast'); // Hide scroll icon
+      $('nav').fadeIn('slow'); // Show nav bar
     }
   })
 
